a318f82d085d7da7d7e58c6e77cb96ad2b2dbdc0
John Horton <john.joseph.horton@gmail.com>
2020-04-03 16:52:35 -0400

    Mostly writeup

---
+++ README.md
@@ -0,0 +1,24 @@
+# About
+
+# Pre-requisites to run
+
+You need one package from John Horton
+
+```
+> library(devtools)
+> instal_github("johnjosephhorton/JJHmisc") 
+
+```
+
+The R packages you need are all available from CRAN.
+If you want a quick and diry way to install
+```
+cd writeup
+make install_packages 
+```
+
+# To build
+```
+cd writeup
+make remote_work.pdf
+```
--- analysis/eda.R
+++ analysis/eda.R
@@ -18,7 +18,8 @@ suppressPackageStartupMessages({
 df.raw <- read.csv("../etl/gcs.csv",
                    stringsAsFactors = FALSE) 
 
-colnames(df.raw) <- c("id", "time", "status", "pub_cat", "gender", "age", "geo", "weight", "q", "response_time")
+colnames(df.raw) <- c("id", "time", "status", "pub_cat",
+                      "gender", "age", "geo", "weight", "q", "response_time")
 
 
 ## Add geograph information 
@@ -39,21 +40,57 @@ addParam("\\LFPRhat", (lfpr %>% multiply_by(100)) %>% formatC(digits = 0, format
 
 addParam("\\numObs", nrow(df.raw) %>% formatC(big.mark = ","))
 addParam("\\numObsWorking", nrow(df.working) %>% formatC(big.mark = ","))
-addParam("\\SurveyStart", df.raw %$% time %>% min)
-addParam("\\SurveyEnd", df.raw %$% time %>% max)
+addParam("\\SurveyStart", df.raw %$% time %>% min %>% as.Date %>% as.character)
+addParam("\\SurveyEnd", df.raw %$% time %>% max %>% as.Date %>% as.character)
 
-###########
-##
-##########
+####################
+## Overall responses 
+####################
 
 df.working.summary <- df.working %>%
     group_by(q) %>%
     summarise(num.obs = n()) %>%
     ungroup %>%
         mutate(frac = num.obs / sum(num.obs)) %>% 
-        mutate(se = sqrt(frac * (1-frac))/sqrt(num.obs))
+    mutate(se = sqrt(frac * (1-frac))/sqrt(num.obs)) %>%
+    mutate(lb = frac - 1.96 * se,
+           ub = frac + 1.96*se)
+
 
 df.working.summary$q <- with(df.working.summary, reorder(q, frac, mean))
+
+GetPE <- function(answer){
+    mu <- df.working.summary %>% filter(q == answer) %$% frac %>% multiply_by(100) %>% formatC(digits = 1, format = "f")
+    lb <- df.working.summary %>% filter(q == answer) %$% lb %>% multiply_by(100) %>% formatC(digits = 1, format = "f")
+    ub <- df.working.summary %>% filter(q == answer) %$% ub %>% multiply_by(100) %>% formatC(digits = 1, format = "f")
+    c("mu" = mu, "lb" = lb, "ub" = ub)
+}
+
+
+laid.off <- GetPE("I have recently been furloughed or laid-off")
+
+addParam("\\LaidOff", laid.off["mu"])
+addParam("\\LaidOffLB", laid.off["lb"])
+addParam("\\LaidOffUB", laid.off["ub"])
+
+
+wfh <- GetPE("Used to commute, now work from home")
+
+addParam("\\WFH", wfh["mu"])
+addParam("\\WFHLB", wfh["lb"])
+addParam("\\WFHUB", wfh["ub"])
+
+alreadyWFH <- GetPE("Used to work from home and still do")
+
+addParam("\\alreadyWFH", alreadyWFH["mu"])
+addParam("\\alreadyWFHLB", alreadyWFH["lb"])
+addParam("\\alreadyWFHUB", alreadyWFH["ub"])
+
+stillCommute <- GetPE("I continue to commute to work")
+
+addParam("\\stillCommute", stillCommute["mu"])
+addParam("\\stillCommuteLB", stillCommute["lb"])
+addParam("\\stillCommuteUB", stillCommute["ub"])
 
 
 g <- ggplot(data = df.working.summary, aes(y = q, x = frac)) +
@@ -68,6 +105,11 @@ g <- ggplot(data = df.working.summary, aes(y = q, x = frac)) +
 JJHmisc::writeImage(g, "working_summary", width = 5, height = 2, path = "../writeup/plots/")
 
 
+############
+## By gender                                         
+############
+
+## TODO: call out in text how many are missing.
 
 df.working.gender <- df.working %>%
     filter(gender != "Unknown") %>% 
@@ -93,11 +135,12 @@ g <- ggplot(data = df.working.gender, aes(x = gender, y = frac, group = q)) +
 
 JJHmisc::writeImage(g, "gender", width = 6, height = 5, path = "../writeup/plots/")
 
-print(g)
+######################
+## By geography/region
+######################
 
+## TODO: Report fraction missing gender
 
-
-## Grab US state
 df.working.region <- df.working %>%
     filter(region != "NA") %>% 
     group_by(q, region) %>%
@@ -107,18 +150,21 @@ df.working.region <- df.working %>%
         mutate(frac = num.obs / sum(num.obs)) %>% 
     mutate(se = sqrt(frac * (1-frac))/sqrt(num.obs)) 
 
-df.working.region$short.q <- with(df.working.region, gsub('(.{1,17})(\\s|$)', '\\1\n', q))
+df.working.region$short.q <- with(df.working.region,
+                                  gsub('(.{1,17})(\\s|$)', '\\1\n', q))
 
-df.working.region$region <- with(df.working.region, factor(region, levels = c("NORTHEAST", "MIDWEST", "WEST", "SOUTH")))
+## TODO: Some reason for this particular ordering
+df.working.region$region <- with(df.working.region,
+                                 factor(region, levels = c("NORTHEAST", "MIDWEST", "WEST", "SOUTH")))
 
 df.working.region$q <- with(df.working.region, reorder(q, frac, mean))
 
-g <- ggplot(data = df.working.region %>% filter(q != "Used to work from home, but now I commute"), aes(x = region, y = frac, group = q)) +
+g <- ggplot(data = df.working.region %>% filter(q != "Used to work from home, but now I commute"),
+            aes(x = region, y = frac, group = q)) +
     geom_line(position =  position_dodge(0.1), alpha = 0.2) +
     geom_point(position = position_dodge(0.1)) + 
     theme_bw() +
     geom_errorbar(aes(ymin = frac - 2*se, ymax = frac + 2*se), width = 0, position = position_dodge(0.1)) + 
-#    geom_text_repel(data = df.working.region %>% filter(region == "WEST"), aes(label = short.q), xlim = c(2, NA), segment.colour = NA) +
     expand_limits(x = 3) + 
     scale_y_continuous(label = scales::percent_format(accuracy = 1)) +
     facet_wrap(~short.q, ncol = 4, scale = "free_y") + 
@@ -126,10 +172,7 @@ g <- ggplot(data = df.working.region %>% filter(q != "Used to work from home, bu
     ylab("% of respondents") +
     theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
-
 JJHmisc::writeImage(g, "region", width = 6.5, height = 2.5, path = "../writeup/plots/")
-
-print(g)
 
 df.working.state <- df.working %>%
     filter(state != "NA") %>% 
@@ -139,7 +182,6 @@ df.working.state <- df.working %>%
     group_by(state) %>% 
         mutate(frac = num.obs / sum(num.obs)) %>% 
     mutate(se = sqrt(frac * (1-frac))/sqrt(num.obs)) 
-
 
 codes <- list(
 "Alabama"="AL",
@@ -193,7 +235,9 @@ codes <- list(
 "Wisconsin"="WI",
 "Wyoming"="WY")
 
-ok.codes <- as.character(codes[names(codes)])
+####################
+## Make Chloropleths
+####################
 
 lc.codes <- codes
 names(lc.codes) <- tolower(names(codes))
@@ -201,36 +245,12 @@ names(lc.codes) <- tolower(names(codes))
 states_map <- map_data("state")
 states_map$state <- with(states_map, as.character(lc.codes[as.character(region)]))
 
-
-df.combo <- df.working.state %>% left_join(states_map)  %>% filter(q != "Used to work from home, but now I commute")
-
-## g <- ggplot(df.combo %>% filter(q=="I continue to commute to work"), aes(long, lat, group = group))+
-##   geom_polygon(aes(fill = frac), color = "white")+
-##   scale_fill_viridis_c(option = "C")+
-##     theme_classic()
-
-## JJHmisc::writeImage(g, "continue_to_commute", width = 6, height = 4, path = "../writeup/plots/")
-
-
-
-## g <- ggplot(df.combo %>% filter(q=="Used to commute, now work from home"), aes(long, lat, group = group))+
-##   geom_polygon(aes(fill = frac), color = "white")+
-##   scale_fill_viridis_c(option = "C")+
-##     theme_classic()
-
-## JJHmisc::writeImage(g, "work_from_home", width = 6, height = 4, path = "../writeup/plots/")
-
-## g <- ggplot(df.combo %>% filter(q=="I have recently been furloughed or laid-off"), aes(long, lat, group = group))+
-##   geom_polygon(aes(fill = frac), color = "white")+
-##   scale_fill_viridis_c(option = "C")+
-##     theme_classic()
+df.combo <- df.working.state %>% left_join(states_map, by = "state")  %>% filter(q != "Used to work from home, but now I commute")
 
 g <- ggplot(df.combo, aes(long, lat, group = group))+
     facet_wrap(~q, ncol = 2, scales = "free") + 
-  geom_polygon(aes(fill = frac), color = "white")+
+    geom_polygon(aes(fill = frac), color = "white")+
     scale_fill_viridis_c(option = "C") +
     theme_bw()
-## +
-##     theme_classic()
 
 JJHmisc::writeImage(g, "geo", width = 8, height = 6, path = "../writeup/plots/")
