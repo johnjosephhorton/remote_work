e42628348a5867217f4d83634c54d91f5547f292
John Horton <john.joseph.horton@gmail.com>
2020-04-04 13:32:47 -0400

    Local state reporting analysis

--- analysis/eda.R
+++ analysis/eda.R
@@ -30,6 +30,11 @@ r <- sapply(df.raw$geo, function(x) as.vector(strsplit(x, split = "-")))
 df.raw$region <- unlist(lapply(r, function(x) x[2]))
 df.raw$state <- unlist(lapply(r, function(x) x[3]))
 
+
+df.lfpr <- df.raw %>% group_by(state) %>%
+    summarise(lfpr = mean(q != "None of the above / Not working for pay"))
+
+
 ## Separate out workers that are out of the labor force 
 df.working <- df.raw %>% filter(q != "None of the above / Not working for pay")
 
@@ -52,12 +57,16 @@ addParam("\\SurveyEnd", df.raw %$% time %>% max %>% as.Date %>% as.character)
 
 df.working.summary <- df.working %>%
     group_by(q) %>%
-    summarise(num.obs = n()) %>%
+    summarise(num.obs = n(),
+              num.obs.wt = sum(weight)) %>%
     ungroup %>%
-        mutate(frac = num.obs / sum(num.obs)) %>% 
+    mutate(frac = num.obs / sum(num.obs),
+           frac.weighted = num.obs.wt / sum(num.obs.wt)) %>% 
     mutate(se = sqrt(frac * (1-frac))/sqrt(num.obs)) %>%
     mutate(lb = frac - 1.96 * se,
            ub = frac + 1.96*se)
+
+## State LFPR estimates 
 
 
 df.working.summary$q <- with(df.working.summary, reorder(q, frac, mean))
@@ -107,6 +116,43 @@ g <- ggplot(data = df.working.summary, aes(y = q, x = frac)) +
 
 JJHmisc::writeImage(g, "working_summary", width = 5, height = 2, path = "../writeup/plots/")
 
+#########
+## By age  
+#########
+
+df.working.age <- df.working %>%
+    filter(age != "Unknown") %>% 
+    group_by(q, age) %>%
+    summarise(num.obs = n(),
+              num.obs.wt = sum(weight)) %>%
+    ungroup %>%
+    group_by(age) %>% 
+    mutate(frac = num.obs / sum(num.obs),
+           frac.weighted = num.obs.wt / sum(num.obs.wt)
+           ) %>% 
+    mutate(se = sqrt(frac * (1-frac))/sqrt(num.obs)) %>%
+    mutate(lb = frac - 1.96 * se,
+           ub = frac + 1.96*se)
+
+
+df.working.age$short.q <- with(df.working.age, gsub('(.{1,15})(\\s|$)', '\\1\n', q))
+
+g <- ggplot(data = df.working.age %>% filter(q != "Used to work from home, but now I commute"), aes(x = age, y = frac, group = q, colour = factor(q))) +
+    geom_point(position = position_dodge(0.3)) +
+    geom_line(position = position_dodge(0.3)) +
+    geom_errorbar(aes(ymin = frac - 1.96*se, ymax = frac + 1.96*se), width = 0, position = position_dodge(0.3)) + 
+    geom_text_repel(data = df.working.age %>% filter(age == "65+") %>% filter(q != "Used to work from home, but now I commute"),
+                    aes(label = short.q), xlim = c(6.25, NA), segment.colour = NA)  +
+    expand_limits(x = 8) + 
+    theme_bw() +
+    theme(legend.position = "none") +
+    scale_y_continuous(label = scales::percent_format(accuracy = 1)) +
+    ylab("% of respondents") +
+    xlab("Age of respondents") 
+
+print(g)
+
+JJHmisc::writeImage(g, "by_age", width = 6, height = 4, path = "../writeup/plots/")
 
 ############
 ## By gender                                         
@@ -164,18 +210,18 @@ df.working.region$q <- with(df.working.region, reorder(q, frac, mean))
 
 g <- ggplot(data = df.working.region %>% filter(q != "Used to work from home, but now I commute"),
             aes(x = region, y = frac, group = q)) +
-    geom_line(position =  position_dodge(0.1), alpha = 0.2) +
+    #geom_line(position =  position_dodge(0.1), alpha = 0.2) +
     geom_point(position = position_dodge(0.1)) + 
     theme_bw() +
     geom_errorbar(aes(ymin = frac - 2*se, ymax = frac + 2*se), width = 0, position = position_dodge(0.1)) + 
     expand_limits(x = 3) + 
     scale_y_continuous(label = scales::percent_format(accuracy = 1)) +
-    facet_wrap(~short.q, ncol = 4, scale = "free_y") + 
+    facet_wrap(~short.q, ncol = 2, scale = "free_y") + 
     xlab("") +
     ylab("% of respondents") +
     theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
-JJHmisc::writeImage(g, "region", width = 6.5, height = 2.5, path = "../writeup/plots/")
+JJHmisc::writeImage(g, "region", width = 5, height = 5, path = "../writeup/plots/")
 
 df.working.state <- df.working %>%
     filter(state != "NA") %>% 
@@ -363,7 +409,8 @@ df.state.combo$q.short <- with(df.state.combo, as.character(short.name[as.charac
 
 df.reg <- df.state.combo %>% select(state, two_week_total, population, frac, q.short) %>% as_tibble %>%
     reshape2::dcast(state + two_week_total + population ~ q.short, value.var = "frac") %>%
-    select(-now_commute)
+    select(-now_commute) %>%
+    left_join(df.lfpr)
 
 m <- lm(wfh ~ commute, data = df.reg)
 
@@ -385,17 +432,25 @@ JJHmisc::writeImage(g, "commute_vs_wfh", width = 6, height = 4, path = "../write
 ## How much variance does
 #############################
 
-m.commute <- lm(log(two_week_total) ~ log(population) + log(commute),
+m.commute <- lm(log(two_week_total) ~ log(population) + log(commute) + log(lfpr),
         data = df.reg)
 
-m.wfh <- lm(log(two_week_total) ~ log(population) + log(wfh),
+m.wfh <- lm(log(two_week_total) ~ log(population) + log(wfh) + log(lfpr),
         data = df.reg)
 
-m.laidoff <- lm(log(two_week_total) ~ log(population) + log(laidoff),
+m.laidoff <- lm(log(two_week_total) ~ log(population) + log(laidoff) + log(lfpr),
         data = df.reg)
 
-m.still_wfh <- lm(log(two_week_total) ~ log(population) + log(still_wfh),
+m.still_wfh <- lm(log(two_week_total) ~ log(population) + log(still_wfh) + log(lfpr) ,
         data = df.reg)
+
+df.reg$r.hat <- exp(with(df.reg, log(two_week_total) - log(population) - log(laidoff) - log(lfpr)) )
+
+df.reg$state <- with(df.reg, reorder(state, r.hat, mean))
+
+ggplot(data = df.reg, aes(y = state, x = r.hat)) + geom_point()
+
+#m <- lmer(log(laidoff) ~ (1|state), data = df.reg)
 
 #stargazer::stargazer(m.commute, m.wfh, m.laidoff, m.still_wfh, type = "text")
 
@@ -403,7 +458,7 @@ out.file <- "../writeup/tables/ui.tex"
 sink("/dev/null")
 s <- stargazer::stargazer(m.commute, m.wfh, m.laidoff, m.still_wfh, 
                           dep.var.labels = c("Log state two week UI claims"), 
-                          covariate.labels = c("Log state population", "Still commuting frac. (log)", "Now WFH frac. (log)", "Laid-off frac. (log)", "Still WFH (log)"), 
+                          covariate.labels = c("Log state population", "Still commuting frac. (log)", "Switch to WFH frac. (log)", "Laid-off frac. (log)", "Still WFH (log)"), 
                           title = "Predicting UI claims by state",
                           no.space = TRUE, 
                           label = "tab:ui",
